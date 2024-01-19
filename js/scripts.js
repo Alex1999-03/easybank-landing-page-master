5e0241b34d5a83d2b4b97f46fd16cb3175b89198
Alex1999-03 <miguel.alexander.rodriguez1999@gmail.com>
2024-01-18 18:47:08 -0600

    fix: set right path

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,11 @@ hamburger.addEventListener('click', (e) => {
   const img = hamburger.firstElementChild
 
   if (!open) {
-    img.src = '../images/icon-close.svg'
+    img.src = './images/icon-close.svg'
     hamburger.dataset.open = true
     navigation.classList.add('header__nav--open')
   } else {
-    img.src = '../images/icon-hamburger.svg'
+    img.src = './images/icon-hamburger.svg'
     hamburger.dataset.open = false
     navigation.classList.remove('header__nav--open')
   }
